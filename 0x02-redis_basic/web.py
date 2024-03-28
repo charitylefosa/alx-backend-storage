a955de23b4ff64087f41affda66e36bec41673b7
charitylefosa <charitylefosa@gmail.com>
2024-03-28 16:32:22 -0700

    save

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -24,7 +24,7 @@ def data_cacher(method: Callable) -> Callable:
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.incr(f'count:{url}')  # Increment count
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
